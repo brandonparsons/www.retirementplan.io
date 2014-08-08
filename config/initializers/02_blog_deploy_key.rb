76ef14fd9eb081880eb81a56189cc7bee268bcec
Brandon Parsons <parsons.brandon@gmail.com>
2014-08-07 20:06:20 -0600

    Ensure directory created

--- config/initializers/02_blog_deploy_key.rb
+++ config/initializers/02_blog_deploy_key.rb
@@ -1,6 +1,7 @@
 require 'fileutils'
 
 key_loc = Rails.root.join(ENV['BLOG_DEPLOY_KEY_LOCATION'])
+FileUtils.mkdir_p key_loc
 
 File.write(key_loc, ENV['BLOG_GIT_SSH_PRIVATE_KEY'])
 File.chmod(0600, key_loc)
